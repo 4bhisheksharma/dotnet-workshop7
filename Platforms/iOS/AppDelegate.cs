032956d4f80def3b0a1921635e1047ba8807b706
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-12-10 11:47:05 +0545

    project initialization

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CollegeWelcome
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
